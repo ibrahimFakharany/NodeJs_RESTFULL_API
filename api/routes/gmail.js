0b8a8721de28d6f87abc0f989073ea1a74fad952
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-02 17:40:18 +0200

    kjfds

--- api/routes/gmail.js
+++ api/routes/gmail.js
@@ -135,10 +135,32 @@ function sendEmail(auth, res_sendEmail) {
         console.log(data.emailAddress);
         var userEmail = data.emailAddress;
 
-        res_sendEmail.status(200).JSON({
-            email : userEmail
+
+        var raw = makeBody(toEmail, userEmail, subjectEmail, bodyEmail);
+        gmail.users.messages.send({
+            auth: auth,
+            userId: 'me',
+            resource: {
+                raw: raw
+            }
+        }, function(err, response) {
+            res.send(err || response)
         });
 
     })
+}
+
+function makeBody(to, from, subject, message) {
+    var str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
+        "MIME-Version: 1.0\n",
+        "Content-Transfer-Encoding: 7bit\n",
+        "to: ", to, "\n",
+        "from: ", from, "\n",
+        "subject: ", subject, "\n\n",
+        message
+    ].join('');
+
+    var encodedMail = new Buffer(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
+        return encodedMail;
 }
 module.exports = router;
