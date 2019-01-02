24921345531e4e3664bbbb4f3f023a93fb0503fc
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-02 12:31:38 +0200

    kljadsf

--- api/routes/gmail.js
+++ api/routes/gmail.js
@@ -7,35 +7,29 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const TOKEN_PATH = 'token.json';
 
 router.get('/', (req, res, next) => {
-    
-    res.status(200).json({
-        speech: "hello from gmail webservice heroku",
-        displayText: "this is display text",
-        source: "this is the source"
-    })
     fs.readFile('credentials.json', (err, content) => {
         if (err) return console.log('Error loading client secret file:', err);
         // Authorize a client with credentials, then call the Gmail API.
-        authorize(JSON.parse(content), listLabels);
+        authorize(JSON.parse(content), listLabels, res);
     });
 });
 
 
-function authorize(credentials, callback) {
+function authorize(credentials, callback, res) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
+        if (err) return getNewToken(oAuth2Client, callback, res);
         oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        callback(oAuth2Client,res);
     });
 };
 
 
-function getNewToken(oAuth2Client, callback) {
+function getNewToken(oAuth2Client, callback, res) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -55,13 +49,13 @@ function getNewToken(oAuth2Client, callback) {
                 if (err) return console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
             });
-            callback(oAuth2Client);
+            callback(oAuth2Client,res);
         });
     });
 }
 
 
-function listLabels(auth) {
+function listLabels(auth, res) {
     const gmail = google.gmail({ version: 'v1', auth });
     gmail.users.labels.list({
         userId: 'me',
@@ -71,10 +65,13 @@ function listLabels(auth) {
         if (labels.length) {
             console.log('Labels:');
             labels.forEach((label) => {
-                console.log(`- ${label.name}`);
+                //console.log(`- ${label.name}`);
+                res.status(200).json({
+                    speech: "hello from gmail webservice heroku",
+                    displayText: "this is display text",
+                    source: "this is the source"
+                });
             });
-
-
         } else {
             console.log('No labels found.');
         }
