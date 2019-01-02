bb01356bf65ea2275a6158397e3a1e753515c57c
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-02 13:00:22 +0200

    klasdjfasd

--- api/routes/gmail.js
+++ api/routes/gmail.js
@@ -6,55 +6,13 @@ const { google } = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const TOKEN_PATH = 'token.json';
 
-router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
     fs.readFile('credentials.json', (err, content) => {
         if (err) return console.log('Error loading client secret file:', err);
         // Authorize a client with credentials, then call the Gmail API.
         authorize(JSON.parse(content), listLabels, res);
     });
 });
-
-
-function authorize(credentials, callback, res) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback, res);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client,res);
-    });
-};
-
-
-function getNewToken(oAuth2Client, callback, res) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client,res);
-        });
-    });
-}
-
-
 function listLabels(auth, res_api) {
     const gmail = google.gmail({ version: 'v1', auth });
     gmail.users.labels.list({
@@ -65,12 +23,12 @@ function listLabels(auth, res_api) {
         if (labels.length) {
             console.log('Labels:');
             labels.forEach((label) => {
-                //console.log(`- ${label.name}`);
-                res_api.status(200).json({
-                    speech: "hello from gmail webservice heroku",
-                    displayText: "this is display text",
-                    source: "this is the source"
-                });
+                console.log(`- ${label.name}`);
+            });
+            res_api.status(200).json({
+                speech: "hello from gmail webservice heroku",
+                displayText: "this is display text",
+                source: "this is the source"
             });
         } else {
             console.log('No labels found.');
@@ -79,7 +37,7 @@ function listLabels(auth, res_api) {
 };
 
 
-function getNewToken(oAuth2Client, callback) {
+function getNewToken(oAuth2Client, callback,res_new_token) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -99,22 +57,22 @@ function getNewToken(oAuth2Client, callback) {
                 if (err) return console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
             });
-            callback(oAuth2Client);
+            callback(oAuth2Client,res_new_token);
         });
     });
 }
 
 
-function authorize(credentials, callback) {
+function authorize(credentials, callback,res_authorize) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
+        if (err) return getNewToken(oAuth2Client, callback,res_authorize);
         oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        callback(oAuth2Client,res_authorize);
     });
 };
 module.exports = router;
