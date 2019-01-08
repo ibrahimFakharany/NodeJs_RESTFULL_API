b724b83bed6a81371633c0b10075beb0ea2fd6a4
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-08 10:43:09 +0200

    fdsf

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 const gmailRoutes = require('./api/routes/gmail');
+const dialogFlow = require('./api/routes/MyDialogFlow')
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/gmail', gmailRoutes);
+app.use('/myDialogFlow', gmailRoutes);
 module.exports = app;
