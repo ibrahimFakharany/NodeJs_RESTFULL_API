41e41ac09e2be26c0169aff59abe89369661115c
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-20 13:15:14 +0200

    fasdfs

--- api/routes/GmailOperations.js
+++ api/routes/GmailOperations.js
@@ -496,7 +496,6 @@ class GmailOperations {
 
     makeBodyForReplying(to, from, messageId, subject, message) {
         var str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
-            "MIME-Version: 1.0\n",
             "Content-Transfer-Encoding: 7bit\n",
             "to: ", to, "\n",
             "from: ", from, "\n",
