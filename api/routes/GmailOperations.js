6c03c25db66d06ab33ffb3f3d49b2126ca6666fb
450 G1 <ibraheemelfakharany@gmail.com>
2019-01-20 16:35:51 +0200

    FADFSD

--- api/routes/GmailOperations.js
+++ api/routes/GmailOperations.js
@@ -388,9 +388,9 @@ class GmailOperations {
         let token = await this.getToken();
         let link = null; 
         if (state == "by") {
-            link = 'https://www.googleapis.com/gmail/v1/users/me/messages?q=from:\"' + contactName + '\"'+subject+'\"&access_token='+ token;
+            link = 'https://www.googleapis.com/gmail/v1/users/me/messages?q=from:\"' + email + '\"'+subject+'\"&access_token='+ token;
         } else if (state == "to") {
-            link = 'https://www.googleapis.com/gmail/v1/users/me/messages?q=to:\"' + contactName + '\"'+subject+'\"&access_token='+ token;
+            link = 'https://www.googleapis.com/gmail/v1/users/me/messages?q=to:\"' + email + '\"'+subject+'\"&access_token='+ token;
         }
         let promise = new Promise((resolve, reject) => {
             request('https://www.googleapis.com/gmail/v1/users/me/messages?q=subject:\"' + subject + '\"&access_token=' + token, { json: true }, (err, res, body) => {
